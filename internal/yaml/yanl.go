ce72f430196ce24895a85dfc3fc6c8e4f5fe7b13
BodeSpezial <konstantin.bode@outlook.com>
2020-06-22 19:47:39 +0200

    add a function to convert a byte-stream to struct changelog

--- internal/yaml/yanl.go
+++ internal/yaml/yanl.go
@@ -1,2 +1,19 @@
 package yaml
 
+import (
+	"chango/structs"
+	"gopkg.in/yaml.v2"
+	"log"
+)
+
+// yamlify unmarshals content to a Changelog var and returns it
+func Yamlify(content []byte) structs.Changelog {
+	chlog := structs.Changelog{}
+
+	err := yaml.Unmarshal(content, &chlog)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	return chlog
+}
