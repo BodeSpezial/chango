2106af516128509386979e644fe40230bca2790e
BodeSpezial <konstantin.bode@outlook.com>
2020-06-22 20:04:16 +0200

    add function to read all changelog files and bundle them into one changelog struct

--- internal/changes/changes.go
+++ internal/changes/changes.go
@@ -1,2 +1,14 @@
 package changes
 
+import (
+	"chango/structs"
+)
+// gatherChanges reads all the files from the changelog folder and returns them in as a Changelog
+func GatherChanges(path string) structs.Changelog {
+	if path == "" {
+		path = "./changelog/"
+	}
+	changes := structs.Changelog{}
+
+	return changes
+}
