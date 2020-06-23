1ae839fe49c42e8ee45335fb8fba22ad6e7fde81
BodeSpezial <konstantin.bode@outlook.com>
2020-06-23 08:20:44 +0200

    open the files and append the found changelog to the Changelog struct

--- internal/changes/changes.go
+++ internal/changes/changes.go
@@ -1,7 +1,9 @@
 package changes
 
 import (
+	"chango/internal/yaml"
 	"chango/structs"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -21,6 +23,20 @@ func GatherChanges(path string) structs.Changelog {
 	list, _ := files.Readdirnames(0)
 	for _, name := range list {
 
+		file, err := os.Open(path + name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		changes.Added = append(changes.Added, yaml.Yamlify(data).Added...)
+		changes.Changed = append(changes.Changed, yaml.Yamlify(data).Changed...)
+		changes.Fixed = append(changes.Fixed, yaml.Yamlify(data).Fixed...)
+		changes.Removed = append(changes.Removed, yaml.Yamlify(data).Removed...)
 	}
 
 	return changes
