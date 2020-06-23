958b87975767dcb725879accd18eb8cb38f012ef
BodeSpezial <konstantin.bode@outlook.com>
2020-06-23 08:19:49 +0200

    open the changelog directory and iterate over all the found files

--- internal/changes/changes.go
+++ internal/changes/changes.go
@@ -2,13 +2,26 @@ package changes
 
 import (
 	"chango/structs"
+	"log"
+	"os"
 )
+
 // gatherChanges reads all the files from the changelog folder and returns them in as a Changelog
 func GatherChanges(path string) structs.Changelog {
 	if path == "" {
 		path = "./changelog/"
 	}
 	changes := structs.Changelog{}
+
+	files, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("failed opening directory: %s", err)
+	}
+	defer files.Close()
+	list, _ := files.Readdirnames(0)
+	for _, name := range list {
+
+	}
 
 	return changes
 }
