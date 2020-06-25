2285569298ecbe0143ca2dd2ac7f248f6569609b
BodeSpezial <konstantin.bode@outlook.com>
2020-06-25 23:54:19 +0200

    add function to create a markdown section from the changelog

--- internal/markdown/markdown.go
+++ internal/markdown/markdown.go
@@ -8,3 +8,14 @@ import (
 	"time"
 )
 
+// addSection adds a section with a list of changes in that section
+func addSection(name string, s *[]structs.ChangelogEntry, m *string) {
+	if len(*s) > 0 {
+		*m += "### " + name + "\n"
+		for _, section := range *s {
+			*m += "  - " + section.Message + " (#" + strconv.Itoa(section.Pr) + ")\n"
+		}
+		*m += "\n"
+	}
+}
+
