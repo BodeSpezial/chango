d5b709da72b114dc30cac409f3e7408d97ef935c
BodeSpezial <konstantin.bode@outlook.com>
2020-06-27 19:23:17 +0200

    add function to get the current checked our branch

--- internal/git/git.go
+++ internal/git/git.go
@@ -5,3 +5,12 @@ import (
 	"os/exec"
 )
 
+func GetCurrentBranch() string {
+	branch, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
+	if err != nil {
+		log.Println(err)
+	}
+	branchName := string(branch)
+	return branchName
+}
+
