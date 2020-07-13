13343404b1b977739ca3184cd4e078f1f5212e64
BodeSpezial <konstantin.bode@outlook.com>
2020-07-13 18:00:19 +0200

    Add subcommands for adding some specific new entries

--- cmd/new.go
+++ cmd/new.go
@@ -10,21 +10,7 @@ import (
 )
 
 var conf = config.ParseConfigFile()
-var newCmd = &cobra.Command{
-	Use:   "new",
-	Short: "Add a new changelog entry",
-	Long: `Calling this command lets you add a new changelog entry. You can to this directly through the command-line or open an editor.
 
-If using an editor you can either give a name or a path through the -E flag or it uses the one specified in your config-file.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		changelogFile := "./" + conf.ChlogFolder + "/" + git.GetCurrentBranch() + ".toml"
-
-		openChlog := exec.Command(conf.Editor, changelogFile)
-		openChlog.Stdin = os.Stdin
-		openChlog.Stdout = os.Stdout
-		if err := openChlog.Run(); err != nil {
-			log.Println(err)
-		}
 
 		if conf.AutoCommit == true {
 			git.CommitChangelog(changelogFile)
@@ -32,6 +18,59 @@ If using an editor you can either give a name or a path through the -E flag or i
 	},
 }
 
+var (
+	newChlog = &cobra.Command{
+		Use:   "new",
+		Short: "Add a new changelog entry",
+		Long: `Calling this command lets you add a new changelog entry. You can to this directly through the command-line or open an editor.
+
+If using an editor you can either give a name or a path through the -E flag or it uses the one specified in your config-file.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			openChlogFile()
+		},
+	}
+	add = &cobra.Command{
+		Use:   "add",
+		Short: "Creates a new changelog entry from type 'Added'",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			addChlogEntryToFile()
+
+			//openChlogFile()
+		}}
+	remove = &cobra.Command{
+		Use:   "remove",
+		Short: "Creates a new changelog entry from type 'Removed'",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			addChlogEntryToFile()
+
+			//openChlogFile()
+		}}
+	change = &cobra.Command{
+		Use:   "change",
+		Short: "Creates a new changelog entry from type 'Changed'",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			addChlogEntryToFile()
+
+			//openChlogFile()
+		}}
+	fix = &cobra.Command{
+		Use:   "fix",
+		Short: "Creates a new changelog entry from type 'Fixed'",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			addChlogEntryToFile()
+
+			//openChlogFile()
+		}}
+)
+
 func init() {
-	rootCmd.AddCommand(newCmd)
+	rootCmd.AddCommand(newChlog)
+	newChlog.AddCommand(add)
+	newChlog.AddCommand(change)
+	newChlog.AddCommand(fix)
+	newChlog.AddCommand(remove)
 }
