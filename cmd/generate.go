e7535421132356955623e339fda837c6fa5942e4
BodeSpezial <konstantin.bode@outlook.com>
2020-08-04 00:20:24 +0200

    Add file actions to generate command

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,9 +1,16 @@
 package cmd
 
 import (
+	"chango/internal/changes"
+	"chango/internal/markdown"
+    "chango/internal/files"
 	"github.com/spf13/cobra"
+	"bufio"
+	"log"
+	"os"
 )
 
+// generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generates the current changelog",
@@ -11,6 +18,21 @@ var generateCmd = &cobra.Command{
 
 After bundling the changelog and appending it to the CHANGELOG.md file (or creating a new one if none is existing) it deletes all of the current changelog entries to start a new release cycle.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		changes := changes.GatherChanges("")
+		md := markdown.GenerateMarkdown(changes)
+
+		file, err := os.Open("./Changelog.md")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		files.InsertStringToFile("./Changelog.md", "<!-- begin:changelog -->\n\n"+md, "<!-- begin:changelog -->")
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
