02d22a36595104868cb3ec267a526dd306dd07ba
BodeSpezial <konstantin.bode@outlook.com>
2020-08-09 21:50:14 +0200

    add var section, change standard respnse on `chango config`

--- cmd/config.go
+++ cmd/config.go
@@ -7,25 +7,24 @@ import (
 	"fmt"
 )
 
-// configCmd represents the config command
-var configCmd = &cobra.Command{
-	Use:   "config",
-	Short: "View or change your configuration settings for chango.",
-	Long: `
+var (
+	configCmd = &cobra.Command{
+		Use:   "config",
+		Short: "View or change your configuration settings for chango.",
+		Long: `
 
-If you run this command for the first time and you don't have a config file yet it creates a new one with the default values.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := viper.ReadInConfig(); err != nil {
-			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-				viper.SafeWriteConfig()
+To create a config file with the default values run chango config init [flags]`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if cfgFile := viper.ConfigFileUsed(); cfgFile != "" {
+				fmt.Println("Using config file:", cfgFile)
 			} else {
-				panic(fmt.Errorf("Fatal error config file: %s \n", err))
+				fmt.Println("No config file used yet.\n")
+				cmd.Help()
 			}
-		}
-		fmt.Println("View or change your configuration settings for chango.")
+		},
+	}
 
-	},
-}
+)
 
 func init() {
 	rootCmd.AddCommand(configCmd)
