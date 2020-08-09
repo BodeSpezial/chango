0fc92f42e78b6c2cd475774d3ee45ec9b1318fea
BodeSpezial <konstantin.bode@outlook.com>
2020-08-09 21:51:21 +0200

    add command to print individual config fields or the path to the used config file

--- cmd/config.go
+++ cmd/config.go
@@ -34,9 +34,27 @@ To create a config file with the default values run chango config init [flags]`,
 		Args: cobra.ExactArgs(2),
 	}
 
+	getCmd = &cobra.Command{
+		Use:   "get",
+		Short: "Print the value of the given field",
+		Run: func(cmd *cobra.Command, args []string) {
+			if args[0] == "path" {
+				if cfgFile := viper.ConfigFileUsed(); cfgFile != "" {
+					fmt.Println("Using config file:", cfgFile)
+				} else {
+					fmt.Println("No config file used yet.")
+				}
+
+			} else {
+				fmt.Println(viper.Get(args[0]))
+			}
+		},
+		Args: cobra.ExactArgs(1),
+	}
 )
 
 func init() {
 	rootCmd.AddCommand(configCmd)
 	configCmd.AddCommand(setCmd)
+	configCmd.AddCommand(getCmd)
 }
